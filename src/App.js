5c1b8d97341b06ff4453a1a4d26770e6a4bc81be
necheporenko <ne4eporenko.v@gmail.com>
2019-08-19 12:58:58 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 
@@ -8,10 +9,12 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+        </Switch>
       </div>
     );
-  }
+  } 
 }
 
 export default App;
