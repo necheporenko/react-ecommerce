d027cbb90aed20b14e15620957e59ad3639b0426
necheporenko <ne4eporenko.v@gmail.com>
2019-08-19 13:44:19 +0300

    feat: add shop page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
 import './App.css';
 
@@ -11,6 +12,7 @@ class App extends React.Component {
       <div className='App'>
         <Switch>
           <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     );
