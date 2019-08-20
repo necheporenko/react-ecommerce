6971632095f8df5497a703989a550a9a6b0e4965
necheporenko <ne4eporenko.v@gmail.com>
2019-08-20 18:44:02 +0300

    feat:  signin page; input & button components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header/header.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -16,6 +17,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
