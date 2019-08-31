6f8f6b478192cac9217c6c42fbd15521c396ec09
necheporenko <ne4eporenko.v@gmail.com>
2019-08-31 13:24:14 +0300

    feat: cart components and reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 
+import cartReducer from './cart/cart.reducer';
 import userReducer from './user/user.reducer';
 
+
 export default combineReducers({
+  cart: cartReducer,
   user: userReducer
 });
