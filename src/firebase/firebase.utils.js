00de9ebc816e47b34b6d28d8ae39a4653c0ef2fe
necheporenko <ne4eporenko.v@gmail.com>
2019-10-03 18:04:43 +0300

    feat: move locat dato into firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyC2lnAS4Pc2qxb33oRyXseszBw1CFV_TYQ",
-  authDomain: "necheporenko-react-shop.firebaseapp.com",
-  databaseURL: "https://necheporenko-react-shop.firebaseio.com",
-  projectId: "necheporenko-react-shop",
-  storageBucket: "necheporenko-react-shop.appspot.com",
-  messagingSenderId: "673917718525",
-  appId: "1:673917718525:web:349ba487d1b10cb7"
+  apiKey: 'AIzaSyC2lnAS4Pc2qxb33oRyXseszBw1CFV_TYQ',
+  authDomain: 'necheporenko-react-shop.firebaseapp.com',
+  databaseURL: 'https://necheporenko-react-shop.firebaseio.com',
+  projectId: 'necheporenko-react-shop',
+  storageBucket: 'necheporenko-react-shop.appspot.com',
+  messagingSenderId: '673917718525',
+  appId: '1:673917718525:web:349ba487d1b10cb7'
 };
 
 firebase.initializeApp(config);
@@ -38,6 +38,37 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
